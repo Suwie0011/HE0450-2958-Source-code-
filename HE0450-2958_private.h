d7753d4774c4418f26c7c99df0c4b4dcadbcd3c4
Suwie <131428030+Suwie0011@users.noreply.github.com>
2023-09-22 11:16:02 +0900

    Add files via upload

---
+++ HE0450-2958_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef HE0450-2958_PRIVATE_H
+#define HE0450-2958_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"6.6.6.6"
+#define VER_MAJOR	6
+#define VER_MINOR	6
+#define VER_RELEASE	6
+#define VER_BUILD	6
+#define COMPANY_NAME	"pankoza"
+#define FILE_VERSION	"6.6.6.6"
+#define FILE_DESCRIPTION	"HE0450-2958 trojan setup"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	"6.6.6.6"
+
+#endif /*HE0450-2958_PRIVATE_H*/
